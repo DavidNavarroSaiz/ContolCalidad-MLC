e32f0eb5c90440c0c618557844092015ff74701d
DavidNavarroSaiz <dcnavarros@unal.edu.co>
2022-08-11 21:20:43 -0500

    rectangle evaluation

--- rectangle_evaluation.py
+++ rectangle_evaluation.py
@@ -1,7 +1,7 @@
 from rectangle_dimensions import RectangleDimensions
 
 
-image_path = './ccompareimages/2junio_10x10.tiff'
+image_path = './compareimages/2junio_10x10.tiff'
 mm_px = 0.2506
 width = RectangleDimensions(image_path)
 width.evaluate_image(mm_px)
