3a74b176aea277c21c8b3777de3f0b4bf82dc240
DavidNavarroSaiz <dcnavarros@unal.edu.co>
2022-11-09 22:32:59 -0500

    line between two points founded

--- sheet_position_evaluation.py
+++ sheet_position_evaluation.py
@@ -1,1 +1,5 @@
-hacer
+from sheet_position import SheetPosition
+
+sheet = SheetPosition('./IMAGENES FORMATO TIFF/PrecisionPosicionHoja/good/10x10.tif')
+sheet.find_white_circle()
+sheet.draw_line()
