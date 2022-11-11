90dbf7c33e7599f5436419dde85fc77a24fbf967
DavidNavarroSaiz <dcnavarros@unal.edu.co>
2022-11-10 21:32:15 -0500

    distances found

--- sheet_position.py
+++ sheet_position.py
@@ -9,7 +9,9 @@ class SheetPosition():
     def __init__(self,path):
         self.img_raw = cv2.imread(path)
         self.img = self.img_raw[:,:,0]
+        self.img = self.img_raw[:,:,0]
         self.imgray = cv2.cvtColor(self.img_raw, cv2.COLOR_BGR2GRAY)
+        self.gray_img = self.imgray
         self.h_imgray, self.w_imgray = self.imgray.shape
 
     def find_rectangle_contour(self):
@@ -47,6 +49,47 @@ class SheetPosition():
                 cX = int(M["m10"] / M["m00"])
                 cY = int(M["m01"] / M["m00"])
                 self.circle_list.append((cX,cY))
+        self.square_center = int((self.circle_list[0][0]+self.circle_list[1][0])/2)
+    def find_gray_levels(self, img):
+        min_value = np.amin(img)
+        max_value = np.amax(img)
+        prom_value = min_value + (max_value - min_value)/2
+        return min_value, max_value, prom_value
+    def find_black_zones_distances(self, number_of_zones):
+        _, max_value, prom_value = self.find_gray_levels(self.gray_img)
+        _, self.thresh = cv2.threshold(self.gray_img, prom_value, max_value, cv2.THRESH_BINARY_INV)
+        contours, _ = cv2.findContours(self.thresh, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+        
+        '''
+        x_values será una lista la cual contiene listas con dos parametros [X, [Y,Z,...]]
+        - X es el valor en x de la gaussiana en esta región 
+        - Y,Z,... son listas que contienen los valores izquierdo y derecho correspondiente al 50% de opacidad en cada placa o subregión
+        '''        
+        x_values = []
+        counter = 0
+        for cnt in contours:  
+            x,y,w,h = cv2.boundingRect(cnt)  # Las variables "x" y "y" son el punto sup izq de la imagen
+            # cv2.rectangle(self.gray_img, (x,y), (x+w,y+h), (0,255,0),2)
+            # cv2.imshow("self.gray_img",self.gray_img)
+            # cv2.waitKey()
+            h_cut = int(h / number_of_zones) # Tamaño de cada división
+            w_cut = int(w*2)             # Se hace una pequeña separación para la imagen
+            dist_izq_list = []                # Lista con el valor d ela gaussiana en cada subregión
+            dist_der_list = []              # Lista de valores correspondientes a Y,Z,...
+        # Se divide cada region en placas ó subregiones, para obtener mayor precisión en las medidas
+        for i in range(number_of_zones):
+            # Sub regiones van de arriba a abajo
+            sub_region = self.thresh[(y+h_cut*i):(y+h_cut*(i+1)), (x):(x+int(w))]
+            contours_sub, _ = cv2.findContours(sub_region, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+            for cnt in contours_sub:            
+                x_sub,y_sub,w_sub,h_sub = cv2.boundingRect(cnt)  # Las variables "x" y "y" son el punto sup izq de la imagen        
+                dist_izq_sub = abs(self.square_center- x_sub)
+                dist_der_sub = abs(self.square_center - (x_sub+w_sub))
+                
+            dist_izq_list.append(dist_izq_sub)  
+            dist_der_list.append(dist_der_sub)  
+            
+        return dist_izq_list,dist_der_list
     def draw_line(self):
         # self.img_line = np.stack((self.imgRoi,)*3, axis=-1)
         img2 = np.zeros_like(self.img_rectangleRGB)
@@ -57,3 +100,5 @@ class SheetPosition():
         cv2.imshow("thresh",img2)
         cv2.waitKey()
         # Hallar distancia a bordes
+
+    
--- sheet_position_evaluation.py
+++ sheet_position_evaluation.py
@@ -1,5 +1,7 @@
 from sheet_position import SheetPosition
 
 sheet = SheetPosition('./IMAGENES FORMATO TIFF/PrecisionPosicionHoja/good/10x10.tif')
-sheet.find_white_circle()
-sheet.draw_line()
+sheet.find_white_circle()  
+izq,der = sheet.find_black_zones_distances(20)                    
+print(izq)
+print(der)
